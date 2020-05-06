a3a88d990d1a88ee5e634bcdd49a1dd9b24c3f28
tim099 <tim11251994@gmail.com>
2020-05-06 17:40:38 +0800

    Update UCL_InputEvent.cs

--- UCL_InputScript/UCL_InputEvent.cs
+++ UCL_InputScript/UCL_InputEvent.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace UCL.InputLib {
+#if UNITY_EDITOR
+    [Core.ATTR.EnableUCLEditor]
+#endif
     public class UCL_InputEvent : MonoBehaviour {
         [System.Serializable]
         public struct KeyDownEvent {
             public KeyCode m_Key;
             public UnityEngine.Events.UnityEvent m_Events;
 #if UNITY_EDITOR
-            [Core.PA.UCL_ButtonProperty("InvokeEvents")] public bool m_InvokeEvent;
+            [Core.PA.UCL_Button("InvokeEvents")] public bool m_InvokeEvent;
 #endif
             public void InvokeEvents() {
                 m_Events?.Invoke();
